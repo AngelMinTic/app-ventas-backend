d51bfd9099d13ebf0e808273601274a279146e46
AngelMinTic <ulvcolombia@gmail.com>
2021-11-26 01:20:43 -0500

    Version 1.0.0 de la aplicación ventas de la tienda tecnológica

---
+++ src/models/venta.model.ts
@@ -0,0 +1,47 @@
+import {Entity, model, property, belongsTo, hasMany} from '@loopback/repository';
+import {Cliente} from './cliente.model';
+import {Producto} from './producto.model';
+
+@model()
+export class Venta extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  estado: number;
+
+  @belongsTo(() => Cliente)
+  clienteId: string;
+
+  @hasMany(() => Producto)
+  productos: Producto[];
+
+  constructor(data?: Partial<Venta>) {
+    super(data);
+  }
+}
+
+export interface VentaRelations {
+  // describe navigational properties here
+}
+
+export type VentaWithRelations = Venta & VentaRelations;
